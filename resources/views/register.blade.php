1178abcb7972907832e776235c1d19dafb4d683e
waxpoo <muhammadruhulbayan.mrb@gmail.com>
2024-11-21 19:06:12 +0700

    hi

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -25,13 +25,6 @@
                 <label for="password_confirmation">Konfirmasi Password:</label>
                 <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required>
             </div>
-            <div class="form-group">
-                <label for="role">Daftar sebagai:</label>
-                <select id="role" name="role" class="form-control">
-                    <option value="user">User</option>
-                    <option value="admin">Admin</option>
-                </select>
-            </div>
             <button type="submit" class="btn btn-primary">Register</button>
         </form>
         <div class="login-link">
